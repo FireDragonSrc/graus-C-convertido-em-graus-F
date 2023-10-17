d0382cb18f5a5a8e441ed746575b08b0b20d2ae3
FireDragonSrc <firedragonsrc@gmail.com>
2023-10-17 11:33:55 -0300

    Commit inicial

---
+++ graus/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propriedade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------conversor de graus para fahrenheit----------");
+            Console.WriteLine("Qual a temperatura em graus?");
+            float graus = int.Parse(Console.ReadLine());
+            float fahrenheit = (9f * graus + 160f) / 5f;
+            Console.WriteLine(fahrenheit);
+            Console.ReadLine();
+        }
+ 
+    }
+}
